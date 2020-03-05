3326d6091504b9028c431090c50e97758a3580f2
Habibi <habibi.dev@gmail.com>
2020-03-05 11:17:11 +0330

    Update Autoload.php
    added init codestar

--- core/class/Autoload.php
+++ core/class/Autoload.php
@@ -3,6 +3,8 @@
 
 namespace wp_panel;
 
+use CSF;
+
 if (!defined('ABSPATH')) exit; // No direct access allowed
 
 /**
@@ -13,12 +15,15 @@ if (!defined('ABSPATH')) exit; // No direct access allowed
 class Autoload
 {
 
+    protected static $prefix = 'wp_panel';
+    protected static $admin_page;
+
+
     static function init()
     {
         add_action('plugins_loaded', 'wp_panel\Autoload::language');
+        add_action('plugins_loaded', 'wp_panel\Autoload::admin_page');
         add_action('admin_enqueue_scripts', 'wp_panel\Autoload::admin_enqueue', 20, 1);
-        add_action('admin_menu', 'wp_panel\Autoload::menu_pages');
-
     }
 
     /**
@@ -40,16 +45,69 @@ class Autoload
         /**
          * Add Style Admin
          */
-        wp_enqueue_style('WP_PANEL', plugins_url('assets/css/admin/core.css', WP_PANEL_FILE), array(), $plugin_info['Version']);
+        wp_enqueue_style('wp-panel', plugins_url('assets/css/admin/core.css', WP_PANEL_FILE), array(), $plugin_info['Version']);
+
+        /**
+         * Add javascript Admin
+         */
+        wp_enqueue_script('wp-panel-js', plugins_url('/assets/js/admin.js', WP_PANEL_FILE), [], $plugin_info['Version']);
 
     }
 
     /**
-     * Install Menu Page
+     * Install Admin Page by codestar
      */
-    static function menu_pages()
+    static function admin_page()
     {
-        add_menu_page(__('Wp Panel', 'WP_PANEL'), __('Wp Panel', 'WP_PANEL'), 'manage_options', 'wp-panel', 'my_menu_output', plugins_url('assets/images/icon.svg', WP_PANEL_FILE), 76);
+        // load CSF
+        include(plugin_dir_path(WP_PANEL_FILE) . '/lib/codestar-framework/classes/setup.class.php');
+
+        // Control core classes for avoid errors
+        if (!class_exists('CSF')) {
+            add_action('admin_notices', function () {
+                self::alert(__('ERROR CLASS EXIST CSF', 'WP_PANEL'));
+            });
+            return;
+        }
+
+
+        // Set Admin Option
+        self::set_plugin_info();
     }
 
+    static function change_copy_right_codestar()
+    {
+        return '';
+    }
+
+    static function set_plugin_info()
+    {
+        CSF::createOptions(self::$prefix, array(
+            'menu_title' => __('Wp Panel', 'WP_PANEL'),
+            'framework_title' => "<img src='" . plugins_url('assets/images/logo.svg', WP_PANEL_FILE) . "' alt=''/>" . __('Wp Panel', 'WP_PANEL'),
+            'menu_slug' => self::$prefix,
+            'menu_icon' => plugins_url('assets/images/icon.svg', WP_PANEL_FILE),
+            'show_bar_menu' => false,
+            'theme' => 'light',
+            'class' => 'wp-panel'
+        ));
+
+        CSF::createSection(self::$prefix, array(
+            'title' => __('Wp Panel', 'WP_PANEL'),
+            'fields' => array()
+        ));
+    }
+
+    /**
+     * @param $msg
+     * @param string $type
+     */
+    public static function alert($msg, $type = 'error')
+    {
+        ?>
+        <div class="notice notice-<?= $type ?> is-dismissible" style="display: block!important">
+            <p><?= $msg ?></p>
+        </div>
+        <?php
+    }
 }
