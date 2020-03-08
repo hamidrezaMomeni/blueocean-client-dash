fc2d9858a432aa16e0a5699b73d154efd5ee9122
Habibi <habibi.dev@gmail.com>
2020-03-08 23:43:27 +0330

    update css register and login

--- core/template/login/page/login.php
+++ core/template/login/page/login.php
@@ -2,7 +2,7 @@
 if (!defined('ABSPATH')) exit; // No direct access allowed
 ?>
 <div class="flexbox-container">
-    <div class="col-lg-6 box-login-register">
+    <div class="col-xl-8 col-11 box-login-register">
         <div class="row m-0">
             <div class="col-lg-6 d-lg-flex d-none text-center align-self-center py-0 image-box">
                 <img src="<?= url_wp_panel('core/template/login/assets/images/login.png') ?>" alt="">
