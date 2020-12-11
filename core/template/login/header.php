3c3d005d0da7a16dba31242591bce81314f9282a
amir hossein habibi <habibi.dev@gmail.com>
2020-12-11 16:43:39 +0330

    Update header.php

--- core/template/login/header.php
+++ core/template/login/header.php
@@ -2,8 +2,8 @@
 if (!defined('ABSPATH')) exit; // No direct access allowed
 global $data;
 
-$favicon = apply_filters('wp_panel_favicon', url_wp_panel('assets/images/logo.svg'));
-$css = apply_filters('wp_panel_css_url_login', url_wp_panel('assets/css/dashboard/login-register.css'));
+$favicon = apply_filters('blue_ocean_cd_favicon', url_blue_ocean_cd('assets/images/logo.svg'));
+$css = apply_filters('blue_ocean_cd_css_url_login', url_blue_ocean_cd('assets/css/dashboard/login-register.css'));
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,7 +12,7 @@ $css = apply_filters('wp_panel_css_url_login', url_wp_panel('assets/css/dashboar
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?= $data['title'] ?></title>
 
-    <link href="<?= url_wp_panel('assets/css/lib/bootstrap/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= url_blue_ocean_cd('assets/css/lib/bootstrap/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?= $css ?>" rel="stylesheet">
 
     <!-- Favicons -->
@@ -21,6 +21,8 @@ $css = apply_filters('wp_panel_css_url_login', url_wp_panel('assets/css/dashboar
     <link rel="icon" href="<?= $favicon ?>" sizes="16x16" type="image/png">
     <link rel="mask-icon" href="<?= $favicon ?>" color="#563d7c">
     <link rel="icon" href="<?= $favicon ?>">
-    <?php do_action('wp_panel_header_login'); ?>
+    <?php
+    do_action('blue_ocean_cd_header', 'login');
+    ?>
 </head>
 <body>
