1b4611d3c439ed9966d82dbf9d979a9816cc2dee
amir hossein habibi <habibi.dev@gmail.com>
2020-12-11 16:43:41 +0330

    Update login.php

--- core/template/login/page/login.php
+++ core/template/login/page/login.php
@@ -5,12 +5,12 @@ if (!defined('ABSPATH')) exit; // No direct access allowed
     <div class="col-xl-8 col-11 box-login-register">
         <div class="row m-0">
             <div class="col-lg-6 d-lg-flex d-none text-center align-self-center py-0 image-box">
-                <img src="<?= url_wp_panel('core/template/login/assets/images/login.png') ?>" alt="">
+                <img src="<?= url_blue_ocean_cd('core/template/login/assets/images/login.png') ?>" alt="">
             </div>
             <div class="col-lg-6 col-12 p-0 form-box form-box-login">
                 <div>
-                    <h2><?= __('Login', 'wp_panel') ?></h2>
-                    <p><?= __('Welcome back, please login to your account.', 'wp_panel') ?></p>
+                    <h2><?= __('Login', 'BLUE_OCEAN_CD') ?></h2>
+                    <p><?= __('Welcome back, please login to your account.', 'BLUE_OCEAN_CD') ?></p>
                 </div>
             </div>
         </div>
