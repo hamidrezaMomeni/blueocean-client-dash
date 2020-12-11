09dc82150c73f7c3363f9c9d7a241544db254ea1
amir hossein habibi <habibi.dev@gmail.com>
2020-12-11 14:00:05 +0330

    Update Autoload.php

--- core/class/Autoload.php
+++ core/class/Autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace wp_panel;
+namespace BlueOceanClientDash;
 
 use CSF;
 
@@ -14,65 +14,65 @@ if (!defined('ABSPATH')) exit; // No direct access allowed
  */
 class Autoload
 {
-    protected static $prefix = 'wp_panel', $slug;
+    protected static $prefix = 'bo_client_dash', $slug;
 
     static function init()
     {
         // includes files
         self::includes();
 
-        add_action('plugins_loaded', 'wp_panel\Autoload::language');
-        add_action('plugins_loaded', 'wp_panel\Autoload::admin_page');
-        add_action('admin_enqueue_scripts', 'wp_panel\Autoload::admin_enqueue', 20, 1);
-        add_action('init', 'wp_panel\Autoload::rewrite');
-        add_action('parse_query', 'wp_panel\Autoload::rewrite_param');
+        add_action('plugins_loaded', 'BlueOceanClientDash\Autoload::language');
+        add_action('plugins_loaded', 'BlueOceanClientDash\Autoload::admin_page');
+        add_action('admin_enqueue_scripts', 'BlueOceanClientDash\Autoload::admin_enqueue', 20, 1);
+        add_action('init', 'BlueOceanClientDash\Autoload::rewrite');
+        add_action('parse_query', 'BlueOceanClientDash\Autoload::rewrite_param');
 
         // set slug
-        self::$slug = get_option_wp_panel('slug') == '' ? 'panel' : get_option_wp_panel('slug');
+        self::$slug = get_option_bo_client_dash('slug') == '' ? 'panel' : get_option_bo_client_dash('slug');
 
     }
 
     static private function includes()
     {
         // load global functions
-        include(plugin_dir_path(WP_PANEL_FILE) . '/core/functions/global.php');
+        include(plugin_dir_path(BO_CLIENT_DASH_FILE) . '/core/functions/global.php');
 
         // load Router
-        include(plugin_dir_path(WP_PANEL_FILE) . '/core/class/Router.php');
+        include(plugin_dir_path(BO_CLIENT_DASH_FILE) . '/core/class/Router.php');
 
     }
 
     static function language()
     {
-        load_plugin_textdomain('WP_PANEL', false, dirname(plugin_basename(WP_PANEL_FILE)) . '/languages');
+        load_plugin_textdomain('BO_CLIENT_DASH', false, dirname(plugin_basename(BO_CLIENT_DASH_FILE)) . '/languages');
     }
 
     static function admin_enqueue()
     {
         // Set Info Plugin
-        $plugin_info = get_plugin_data(WP_PANEL_FILE);
+        $plugin_info = get_plugin_data(BO_CLIENT_DASH_FILE);
 
         /**
          * Add Style Admin
          */
-        wp_enqueue_style('wp-panel', plugins_url('assets/css/admin/core.css', WP_PANEL_FILE), array(), $plugin_info['Version']);
+        wp_enqueue_style('wp-panel', plugins_url('assets/css/admin/core.css', BO_CLIENT_DASH_FILE), array(), $plugin_info['Version']);
 
         /**
          * Add javascript Admin
          */
-        wp_enqueue_script('wp-panel-js', plugins_url('/assets/js/admin.js', WP_PANEL_FILE), [], $plugin_info['Version']);
+        wp_enqueue_script('wp-panel-js', plugins_url('/assets/js/admin.js', BO_CLIENT_DASH_FILE), [], $plugin_info['Version']);
 
     }
 
     static function admin_page()
     {
         // load CSF
-        include(plugin_dir_path(WP_PANEL_FILE) . '/lib/codestar-framework/classes/setup.class.php');
+        include(plugin_dir_path(BO_CLIENT_DASH_FILE) . '/lib/codestar-framework/classes/setup.class.php');
 
         // Control core classes for avoid errors
         if (!class_exists('CSF')) {
             add_action('admin_notices', function () {
-                self::alert(__('ERROR CLASS EXIST CSF', 'WP_PANEL'));
+                self::alert(__('ERROR CLASS EXIST CSF', 'BO_CLIENT_DASH'));
             });
             return;
         }
@@ -85,10 +85,10 @@ class Autoload
     static function set_plugin_info()
     {
         CSF::createOptions(self::$prefix, array(
-            'menu_title' => __('Wp Panel', 'WP_PANEL'),
-            'framework_title' => "<img src='" . plugins_url('assets/images/logo.svg', WP_PANEL_FILE) . "' alt=''/>" . __('Wp Panel', 'WP_PANEL'),
+            'menu_title' => __('Wp Panel', 'BO_CLIENT_DASH'),
+            'framework_title' => "<img src='" . plugins_url('assets/images/logo.svg', BO_CLIENT_DASH_FILE) . "' alt=''/>" . __('Wp Panel', 'BO_CLIENT_DASH'),
             'menu_slug' => self::$prefix,
-            'menu_icon' => plugins_url('assets/images/icon.svg', WP_PANEL_FILE),
+            'menu_icon' => plugins_url('assets/images/icon.svg', BO_CLIENT_DASH_FILE),
             'show_bar_menu' => false,
             'theme' => 'light',
             'class' => 'wp-panel'
@@ -103,29 +103,29 @@ class Autoload
     {
 
         CSF::createSection(self::$prefix, array(
-            'title' => __('Basic Settings', 'WP_PANEL'),
+            'title' => __('Basic Settings', 'BO_CLIENT_DASH'),
             'menu_hidden' => true,
             'fields' => array(
                 // slug
                 array(
                     'id' => 'slug',
                     'type' => 'text',
-                    'title' => __('Wp Panel Address', 'WP_PANEL'),
-                    'placeholder' => __('Wp Panel Address', 'WP_PANEL'),
+                    'title' => __('Wp Panel Address', 'BO_CLIENT_DASH'),
+                    'placeholder' => __('Wp Panel Address', 'BO_CLIENT_DASH'),
                     'default' => 'panel',
                 ),
             )
         ));
         CSF::createSection(self::$prefix, array(
-            'title' => __('Other Settings', 'WP_PANEL'),
+            'title' => __('Other Settings', 'BO_CLIENT_DASH'),
             'menu_hidden' => true,
             'fields' => array(
                 // slug
                 array(
                     'id' => 'slug',
                     'type' => 'text',
-                    'title' => __('Wp Panel Address', 'WP_PANEL'),
-                    'placeholder' => __('Wp Panel Address', 'WP_PANEL'),
+                    'title' => __('Wp Panel Address', 'BO_CLIENT_DASH'),
+                    'placeholder' => __('Wp Panel Address', 'BO_CLIENT_DASH'),
                     'default' => 'panel',
                 ),
             )
