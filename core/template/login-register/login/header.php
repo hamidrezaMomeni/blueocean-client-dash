d16fd2607a24e4c5835acfd2eac6c524ecb59ec6
amir hossein habibi <habibi.dev@gmail.com>
2020-12-17 12:24:47 +0330

    Refactor

--- core/template/login-register/login/header.php
+++ core/template/login-register/login/header.php
@@ -14,7 +14,7 @@ $favicon = apply_filters('blue_ocean_cd_favicon', url_blue_ocean_cd('assets/imag
     <title><?= apply_filters('title', __('Log in', 'BLUE_OCEAN_CD'), 'login') ?></title>
 
     <!-- Styles -->
-    <link href="<?= url_blue_ocean_cd('core/template/default/assets/css/login.css') ?>" rel="stylesheet">
+    <link href="<?= url_blue_ocean_cd('assets/css/dashboard/login-register.css') ?>" rel="stylesheet">
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" href="<?= $favicon ?>" sizes="180x180">
--- core/template/login-register/login/login.php
+++ core/template/login-register/login/login.php
@@ -1,3 +1,6 @@
 <?php
 if (!defined('ABSPATH')) exit; // No direct access allowed
 ?>
+<div>
+
+</div>
