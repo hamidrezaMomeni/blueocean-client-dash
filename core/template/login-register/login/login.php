ab862c7e3d063424a37e70baefc5cbb7b349bfbb
amir hossein habibi <habibi.dev@gmail.com>
2020-12-26 14:44:07 +0330

    Update login.php

--- core/template/login-register/login/login.php
+++ core/template/login-register/login/login.php
@@ -1,54 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit; // No direct access allowed
 
-$data = [
-    'title' => __('Log in.', 'BLUE_OCEAN_CD'),
-    'desc' => __('Log in with you data that you entered during your registration.', 'BLUE_OCEAN_CD'),
-    'remember' => __('keep me logged in', 'BLUE_OCEAN_CD'),
-    'dont-acc' => __("Don't have an account?", 'BLUE_OCEAN_CD'),
-    'sign-up' => __("Sign up", 'BLUE_OCEAN_CD'),
-    'sign-in' => __("Log in", 'BLUE_OCEAN_CD'),
-    'forgot-pass' => __("Forgot password?", 'BLUE_OCEAN_CD'),
-    'background' => url_blue_ocean_cd('assets/images/bg-login.png'),
-    'fields' => [
-        [
-            'id' => 'username',
-            'type' => 'text',
-            'placeholder' => __('name@domain.com', 'BLUE_OCEAN_CD'),
-            'label' => __('E-mail / Username', 'BLUE_OCEAN_CD'),
-        ],
-        [
-            'id' => 'password',
-            'type' => 'password',
-            'placeholder' => __('at least 8 characters', 'BLUE_OCEAN_CD'),
-            'label' => __('Password', 'BLUE_OCEAN_CD'),
-        ]
-    ],
-    'sliders' => [
-        [
-            'title' => __('Account Benefits', 'BLUE_OCEAN_CD'),
-            'desc' => __('Manage your newsletter subscription<br>Insight into your orders including shipment information<br>Save your user data and settings<br>', 'BLUE_OCEAN_CD'),
-            'img' => url_blue_ocean_cd('assets/images/slider.png')
-        ],
-        [
-            'title' => __('Account Benefits', 'BLUE_OCEAN_CD'),
-            'desc' => __('Manage your newsletter subscription<br>Insight into your orders including shipment information<br>Save your user data and settings<br>', 'BLUE_OCEAN_CD'),
-            'img' => url_blue_ocean_cd('assets/images/slider.png')
-        ],
-        [
-            'title' => __('Account Benefits', 'BLUE_OCEAN_CD'),
-            'desc' => __('Manage your newsletter subscription<br>Insight into your orders including shipment information<br>Save your user data and settings<br>', 'BLUE_OCEAN_CD'),
-            'img' => url_blue_ocean_cd('assets/images/slider.png')
-        ],
-        [
-            'title' => __('Account Benefits', 'BLUE_OCEAN_CD'),
-            'desc' => __('Manage your newsletter subscription<br>Insight into your orders including shipment information<br>Save your user data and settings<br>', 'BLUE_OCEAN_CD'),
-            'img' => url_blue_ocean_cd('assets/images/slider.png')
-        ]
-    ]
-];
-
-$data['fields'] = apply_filters('blue_ocean_cd_fields_login', $data['fields']);
+$data = \BlueOceanClientDash\Login::data();
 
 ?>
 <div class="flexbox-container">
