dbd2735b43d2f44be0681215e1dcd135ae685a4a
amir hossein habibi <habibi.dev@gmail.com>
2020-12-26 15:03:19 +0330

    Update index.php

--- core/template/login-register/login/index.php
+++ core/template/login-register/login/index.php
@@ -1,9 +1,31 @@
 <?php
 if (!defined('ABSPATH')) exit; // No direct access allowed
 global $data;
+$favicon = apply_filters('blue_ocean_cd_favicon', url_blue_ocean_cd('assets/images/logo.svg'));
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-include(plugin_dir_path(__FILE__) . '/header.php');
+    <title><?= apply_filters('title', __('Log in', 'BLUE_OCEAN_CD'), 'login') ?></title>
 
-include(plugin_dir_path(__FILE__) . '/login.php');
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" href="<?= $favicon ?>" sizes="180x180">
+    <link rel="icon" href="<?= $favicon ?>" sizes="32x32" type="image/png">
+    <link rel="icon" href="<?= $favicon ?>" sizes="16x16" type="image/png">
+    <link rel="mask-icon" href="<?= $favicon ?>" color="#563d7c">
+    <link rel="icon" href="<?= $favicon ?>">
 
-include(plugin_dir_path(__FILE__) . '/footer.php');
+    <?php
+    do_action('blue_ocean_cd_header', 'login');
+    ?>
+</head>
+<body>
+<?php include(plugin_dir_path(__FILE__) . '/login.php'); ?>
+
+<?php do_action('blue_ocean_cd_footer', 'login'); ?>
+</body>
+</html>
