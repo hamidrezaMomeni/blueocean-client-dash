607696add528a433838d5222e275caa16dc8a81d
amir hossein habibi <habibi.dev@gmail.com>
2020-12-26 14:59:22 +0330

    Update footer.php

--- core/template/login-register/login/footer.php
+++ core/template/login-register/login/footer.php
@@ -1,8 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit; // No direct access allowed
-?>
-<script src="<?= url_blue_ocean_cd('assets/js/login.js') ?>"></script>
-<?php do_action('blue_ocean_cd_footer', 'login'); ?>
+
+ do_action('blue_ocean_cd_footer', 'login'); ?>
 </body>
 
 </html>
